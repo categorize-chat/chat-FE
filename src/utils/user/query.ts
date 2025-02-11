853bed17292c33f44077e7c0493cf83778c01084
monognuisy <ysmsmart1@gmail.com>
2025-02-11 16:56:37 +0900

    refactor: add api response default handler

--- src/utils/user/query.ts
+++ src/utils/user/query.ts
@@ -1,18 +1,11 @@
 import { TUserInfoResponse, TUserJoinRequest, TUserJoinResponse } from './type';
-import { API } from '../api';
+import { API, defaultResponseHandler } from '../api';
 
 export const userJoinQuery = () => ({
   mutationFn: async (req: TUserJoinRequest) => {
     return await API.json
       .post('/user', req)
-      .then(res => res.data as TUserJoinResponse)
-      .then(({ code, message, result }) => {
-        if (code !== 200) {
-          throw new Error(message);
-        }
-
-        return result;
-      });
+      .then(defaultResponseHandler<TUserJoinResponse>);
   },
   refetchOnWindowFocus: false,
 });
@@ -22,14 +15,7 @@ export const userInfoQuery = (id: string) => ({
   queryFn: async () => {
     return await API.json
       .get(`/user/${id}`)
-      .then(res => res.data as TUserInfoResponse)
-      .then(({ code, message, result }) => {
-        if (code !== 200) {
-          throw new Error(message);
-        }
-
-        return result;
-      });
+      .then(defaultResponseHandler<TUserInfoResponse>);
   },
   refetchOnWindowFocus: false,
 });
